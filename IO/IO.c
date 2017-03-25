df374de903033db1f92892643d5156c75fcdf28e
max1018 <maximilian.j.ellsberry@wmich.edu>
2017-03-25 13:50:53 -0400

    Add files via upload
    This IO.c includes the prompt parser test

--- IO/IO.c
+++ IO/IO.c
@@ -1,29 +1,66 @@
-/*
- This file is used to get the input by the user and parses the arguments it also makes sure that the input is valid 
-*/
 #include <stdio.h>
+#include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
+#include <sys/types.h>
 #include <ctype.h>
-#include "IO.h"
 
-void promptParser(float abc[]){
-	int counter = 0;
-	double digit;
-	char usrIn [100];
-	while(counter != 3){
-	    printf("Please enter a number greater than 0: ");
-	    scanf("%s",usrIn );
-	   	printf("%s\n",usrIn);
-	    digit = strtod(usrIn,NULL);
-	    if(digit != 0){
-		    //printf("This works! %lf\n",digit);
-		    abc[counter] = digit;
-		    counter++;
-		}
-		else{
-			printf("This was an invalid input.Try again!\n");
-		}
-	}
+float [] promptParser();
+float [] promptParserTest();
+
+
+float [] promptParser(){
+float abc[3];
+int counter = 0;
+double digit;
+char *token;
+char usrIn [100];
+char *usrPtr;
+  while(counter != 3){
+    printf("Please enter a number greater than 0: ");
+    scanf("%s",usrIn );
+    printf("%s\n",usrIn);
+    digit = strtod(usrIn,NULL);
+
+    if(digit != 0){
+    printf("This works! %lf\n",digit);
+    abc[counter] = digit;
+    counter++;
+  }
+  else{
+    printf("This was an invalid input.Try again!\n");
+  }
+
+ }
+return abc;
+}
+
+promptParserTest(){
+FILE * fp;
+fp = fopen("TestFile.txt","r");
+float abc[3];
+int counter = 0;
+double digit;
+char *token;
+char usrIn [100];
+char *usrPtr;
+while(fgets(usrIn,sizeof(usrIn),fp)!=NULL){
+  while(counter != 3){
+    printf("Please enter a number greater than 0: ");
+    fgets(usrIn,sizeof(usrIn),fp);
+    digit = strtod(usrIn,NULL);
+
+    if(digit > 0){
+    printf("This works! %lf\n",digit);
+    abc[counter] = digit;
+    counter++;
+  }
+  else{
+    printf("This was an invalid input.Try again!\n");
+  }
+
+ }
+}
 }
